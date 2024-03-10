5639b8587665f0f895d4a632dab8cd8490df39af
abhijeet8900 <abhijeet8900@gmail.com>
2024-03-11 01:55:03 +0530

    - Add: Horizontal bar chart component

---
+++ src/components/widgets/horizontal-bar-chart/index.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import PropTypes from "prop-types";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import styles from "./horizontalBarChart.module.css";
+import cx from "classnames";
+import Widget from "../../common/widget";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const DEFAULT_HORIZONTAL_BARCHART_OPTIONS = {
+  indexAxis: "y",
+  type: "bar",
+  interaction: {
+    intersect: false,
+  },
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true
+    }
+  },
+  responsive: true,
+  grouped: true,
+  maintainAspectRatio: false, // Set false for responsiveness
+  plugins: {
+    legend: {
+      position: "bottom",
+    },
+  },
+  
+};
+
+function HorizontalBarChart({ className, title, data }) {
+  let options = {
+    ...DEFAULT_HORIZONTAL_BARCHART_OPTIONS,
+  };
+
+  return (
+    <Widget className={cx(className)} title={title}>
+      <Bar
+        className={cx(styles["horizontal-bar-chart-wrapper"])}
+        options={options}
+        data={data}
+      />
+    </Widget>
+  );
+}
+
+HorizontalBarChart.propTypes = {
+  title: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  data: PropTypes.shape({
+    datasets: PropTypes.arrayOf(PropTypes.object),
+    label: PropTypes.array,
+  }).isRequired,
+};
+
+export default HorizontalBarChart;
