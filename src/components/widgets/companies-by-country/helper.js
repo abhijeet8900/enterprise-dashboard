b73556b1d8a8140ab76efcb541053acdbcb1218b
abhijeet8900 <abhijeet8900@gmail.com>
2024-03-11 20:08:25 +0530

    - Update: Dataset transformation

--- src/components/widgets/companies-by-country/helper.js
+++ src/components/widgets/companies-by-country/helper.js
@@ -1,16 +1,27 @@
+import sortBy from "lodash/sortBy";
 import groupBy from "lodash/groupBy";
 
 export const transformDataset = (data) => {
   const byCategory = groupBy(data, "country");
   const arr = [];
 
+  let totalOfLowestCount = 0;
+
   Object.keys(byCategory).forEach((key) => {
     const obj = byCategory[key];
-    arr.push({ label: key, value: obj.length });
+    if (obj.length > 2) {
+      arr.push({ label: key, value: obj.length });
+    } else {
+      totalOfLowestCount += 1;
+    }
   });
 
+  const sortedArray = sortBy(arr, "value").reverse();
+  if (totalOfLowestCount > 0) {
+    sortedArray.push({ label: "Others", value: totalOfLowestCount });
+  }
   return {
-    xAxis: arr.map((d) => d.label),
-    yAxis: arr.map((d) => d.value),
+    xAxis: sortedArray.map((d) => d.label),
+    yAxis: sortedArray.map((d) => d.value),
   };
 };
